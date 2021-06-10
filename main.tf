2cd6dad55779327970717543d3291fbcd5e3774f
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2021-06-10 00:20:56 -0300

    fix format of code

--- main.tf
+++ main.tf
@@ -64,14 +64,14 @@ resource "aws_vpc" "jenkins" {
 }
 
 resource "aws_subnet" "guiJenkins" {
-  vpc_id = aws_vpc.jenkins.id
+  vpc_id            = aws_vpc.jenkins.id
   availability_zone = "sa-east-1a"
-  cidr_block = "10.0.1.0/24"
+  cidr_block        = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "buildersJenkins" {
-  vpc_id = aws_vpc.jenkins.id
+  vpc_id            = aws_vpc.jenkins.id
   availability_zone = "sa-east-1b"
-  cidr_block = "10.0.2.0/24"
+  cidr_block        = "10.0.2.0/24"
 }
 
