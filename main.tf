28d902bda33deb294482b792c3a898527767be04
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2021-06-16 03:02:47 -0300

    integrate modules on main

--- main.tf
+++ main.tf
@@ -53,28 +53,12 @@ resource "aws_iam_user" "petrolifero" {
   name = "petrolifero"
 }
 
-resource "aws_vpc" "jenkins" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
 
-  tags = {
-    "terraform" = ""
-    "component" = "CI"
-  }
+module "CI" {
+  source = "./CI"
 }
 
-resource "aws_subnet" "guiJenkins" {
-  vpc_id            = aws_vpc.jenkins.id
-  availability_zone = "sa-east-1a"
-  cidr_block        = "10.0.1.0/24"
+module "authentication" {
+  source = "./ldap"
 }
 
-resource "aws_subnet" "buildersJenkins" {
-  vpc_id            = aws_vpc.jenkins.id
-  availability_zone = "sa-east-1b"
-  cidr_block        = "10.0.2.0/24"
-}
-
-resource "aws_iam_user" "HR" {
-  name = "HR"
-}
