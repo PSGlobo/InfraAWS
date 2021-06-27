f8920b6f4cdfa27165b4d5f6a12076d8945f4db1
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2021-06-27 05:42:54 -0300

    add more vpn necessary resources

--- terraform/ldap/main.tf
+++ terraform/ldap/main.tf
@@ -24,12 +24,6 @@ resource "aws_subnet" "ldap2" {
   cidr_block        = "10.1.2.0/24"
 }
 
-resource "aws_subnet" "PKIRoot" {
-  vpc_id            = aws_vpc.ldap.id
-  availability_zone = "sa-east-1c"
-  cidr_block        = "10.1.3.0/24"
-}
-
 resource "aws_directory_service_directory" "ldap" {
   name     = "corp.bbbfake.com"
   password = "SuperSecretPassw0rd"
@@ -47,6 +41,13 @@ resource "aws_directory_service_directory" "ldap" {
   }
 }
 
+
+resource "aws_subnet" "PKIRoot" {
+  vpc_id            = aws_vpc.ldap.id
+  availability_zone = "sa-east-1c"
+  cidr_block        = "10.1.3.0/24"
+}
+
 resource "aws_key_pair" "PKIKeyPair" {
   key_name   = "MicrosoftPKIKeyPair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCW2Ag0ts+mwcni0K/HoGSj7dnNRoynDWMD+Lq6rT6XQCPK4LrFZXI02GzCkSTd/h3PWSLvk8EjSVIH3P8jy5ucdq12p8Nqf9z5l3y+26F91FUYd1X2ZAageEBqEnnpGR+EEqd3Rff8/aer62Hg310hsxRsKzctXTKoAsE8qcIBCrJ9QE21Rg+LuSNn/3i/7HkUNqvhslAzCd73aO5mR4pYRY0oBzTuzAW1j3w1rfvmtenBr9AyvVSWTdfHJvmKVHJ6SaDYQBr2q7TLmo//T/jkAUU9UzDwoS3upzHGeM85cCH6AIFtDnsyUiejYVqMaCZM4Hcvkj9jCASBkhgwfECn"
@@ -54,6 +55,28 @@ resource "aws_key_pair" "PKIKeyPair" {
     "terraform" = "no"
   }
 }
+
+resource "aws_internet_gateway" "accessToVpnLDAP" {
+  vpc_id = aws_vpc.ldap.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "bbbfake.com"
+  validation_method = "DNS"
+
+  tags = {
+    "terraform" = "yes"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 
 resource "aws_instance" "microsoftPKIRoot" {
   ami               = "ami-03ea5c5403c5bcc24"
@@ -64,8 +87,4 @@ resource "aws_instance" "microsoftPKIRoot" {
   tags = {
     "terraform" = "yes"
   }
-}
-
-resource "aws_s3_bucket" "keyPair" {
-  bucket = "keyPairReceiver"
 }
